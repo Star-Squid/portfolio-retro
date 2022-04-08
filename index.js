3e9d490b934ec233879126ae78ec081a98c457bd
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2022-04-08 13:47:22 +0100

    Adjust corkboard width at small sizes

--- index.js
+++ index.js
@@ -1,62 +1,83 @@
 let decorations = ".square, .bubble, .triangle, .circle-two, .circle, .list, .icons";
+let corkboardWidth = $( ".corkboard" ).width();
 
 $( "#cv" ).hover(function() {
+    $( ".corkboard" ).width(corkboardWidth + "px");
+    $( ".corkboard" ).toggleClass("panel");
     $( ".corkboard" ).toggleClass("cv");
-    $( decorations).toggleClass("disappear");
+    $(decorations).toggleClass("disappear");
     $( ".boot, .js, .jq, .languages").toggleClass("disappear");
     $( ".date" ).text("finished March 2022");
 });
 
-
-
 $( "#flashcards" ).hover(function() {
+    $( ".corkboard" ).width(corkboardWidth + "px");
+    $( ".corkboard" ).toggleClass("panel");
     $( ".corkboard" ).toggleClass("flashcards");
-    $( decorations).toggleClass("disappear");
+    $(decorations).toggleClass("disappear");
     $( ".boot, .languages").toggleClass("disappear");
     $( ".date" ).text("finished March 2022");
 });
 
-
 $( "#memory" ).hover(function() {
+    $( ".corkboard" ).width(corkboardWidth + "px");
+    $( ".corkboard" ).toggleClass("panel");
     $( ".corkboard" ).toggleClass("memory");
-    $( decorations).toggleClass("disappear");
+    $(decorations).toggleClass("disappear");
     $( ".boot, .languages").toggleClass("disappear");
     $( ".date" ).text("finished March 2022");
 });
 
 $( "#clock" ).hover(function() {
+    $( ".corkboard" ).width(corkboardWidth + "px");
+    $( ".corkboard" ).toggleClass("panel");
     $( ".corkboard" ).toggleClass("clock");
-    $( decorations).toggleClass("disappear");
+    $(decorations).toggleClass("disappear");
     $( ".boot, .languages").toggleClass("disappear");
     $( ".date" ).text("finished February 2022");
 });
 
 $( "#art" ).hover(function() {
+    $( ".corkboard" ).width(corkboardWidth + "px");
+    $( ".corkboard" ).toggleClass("panel");
     $( ".corkboard" ).toggleClass("art");
-    $( decorations).toggleClass("disappear");
+    $(decorations).toggleClass("disappear");
     $( ".boot, .languages").toggleClass("disappear");
     $( ".date" ).text("finished March 2022");
 });
 
-
 $( "#hookd" ).hover(function() {
+    $( ".corkboard" ).width(corkboardWidth + "px");
+    $( ".corkboard" ).toggleClass("panel");
     $( ".corkboard" ).toggleClass("hookd");
-    $( decorations).toggleClass("disappear");
+    $(decorations).toggleClass("disappear");
     $(".js, .jq, .languages").toggleClass("disappear");
     $( ".date" ).text("finished February 2022");
 });
 
 $( "#highfive" ).hover(function() {
+    $( ".corkboard" ).width(corkboardWidth + "px");
+    $( ".corkboard" ).toggleClass("panel");
     $( ".corkboard" ).toggleClass("highfive");
-    $( decorations).toggleClass("disappear");
+    $(decorations).toggleClass("disappear");
     $( ".boot, .languages").toggleClass("disappear");
     $( ".date" ).text("finished December 2021");
 });
 
+$( "#welcome" ).hover(function() {
+    $( ".corkboard" ).width(corkboardWidth + "px");
+    $( ".corkboard" ).toggleClass("panel");
+    $( ".corkboard" ).toggleClass("welcome");
+    $(decorations).toggleClass("disappear");
+    $( ".boot, .js, .jq, .languages").toggleClass("disappear");
+    $( ".date" ).text("finished April 2022");
+});
 
 $( "#artlinks" ).hover(function() {
+    $( ".corkboard" ).width(corkboardWidth + "px");
+    $( ".corkboard" ).toggleClass("panel");
     $( ".corkboard" ).toggleClass("artlinks");
-    $( decorations).toggleClass("disappear");
+    $(decorations).toggleClass("disappear");
     $( ".boot, .js, .jq, .languages").toggleClass("disappear");
     $( ".date" ).text("finished December 2021");
 });
